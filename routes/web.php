b1cab22b5f23398f45893f45d75479c059883108
Ivan Mandic <ivan.mandic@predavaci.algebra.hr>
2023-10-13 20:06:45 +0000

    dodatak za autorizaciju

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,7 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+Route::middelware(['auth','role:admin'])->group(function(){
+
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
